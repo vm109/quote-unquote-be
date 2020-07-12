2c0ec6b6f9dc1fa8964f414f8ad1097e27c4ec30
vm109 <mangina.venkata@gmail.com>
2020-07-12 21:54:32 +0530

    1.deleted node modules 2. added module for polly

--- services/theHindu.js
+++ services/theHindu.js
@@ -1,4 +1,5 @@
 const feed = require('rss-to-json')
+const polly_reader = require('../domain_services/polly_reader')
 
 const theHindu_rss = {
     ap: 'https://www.thehindu.com/news/national/andhra-pradesh/feeder/default.rss',
@@ -6,8 +7,14 @@ const theHindu_rss = {
 }
 
 const read_hindu = async (category)=>{
+    try{
     const rss = await feed.load(theHindu_rss[category])
+    await polly_reader.speak(rss.items[0]['title'])
     return rss
+    }catch(e){
+        console.log(e)
+        return new Error(e)
+    }
 }
 
 module.exports = {
